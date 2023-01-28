d32349242f9759fd20b67165d0a1822b393c8d10
Kornkawin Bijayayothin <outsource1456@setth.onmicrosoft.com>
2023-01-28 15:25:02 +0700

    [Add] mui and buy feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import {AppBar, Button, Chip, Stack, Toolbar, Typography} from '@mui/material';
+import {
+    AppBar, Toolbar, Typography, Button, Chip, Stack, Container, Card,
+    CardContent, TextField, Divider
+} from '@mui/material';
 import {initializeConnector} from '@web3-react/core'
 import {MetaMask} from '@web3-react/metamask'
 import { ethers } from 'ethers';
-import { formatEther } from '@ethersproject/units';
+import { formatEther, parseUnits } from '@ethersproject/units';
 import abi from './abi.json';
 
 // copy from https://testnet.reiscan.com/address/0x04294758F2C9B32854486843992647fcfBCBA5fE
@@ -22,21 +25,22 @@ function App() {
     const isActive = useIsActive()
     const provider = useProvider()
     const [balance, setBalance] = useState("")
+    const [reiValue, setReiValue] = useState(0)
     useEffect(() => {
         void metamask.connectEagerly()
     }, [])
-    useEffect(() => {
-        const fetchBalance = async () => {
-            try {
-                const signer = provider.getSigner()
-                const smartContract = new ethers.Contract(contractAddress, abi, signer)
-                const myBalance = await smartContract.balanceOf(accounts[0])
-                // convert unit from wei to ether
-                setBalance(formatEther(myBalance))
-            } catch(err) {
-                console.log(err)
-            }
+    const fetchBalance = async () => {
+        try {
+            const signer = provider.getSigner()
+            const smartContract = new ethers.Contract(contractAddress, abi, signer)
+            // wait for balance data
+            const myBalance = await smartContract.balanceOf(accounts[0])
+            setBalance(formatEther(myBalance))
+        } catch(err) {
+            console.log(err)
         }
+    }
+    useEffect(() => {
         if (isActive) {
             // Fetch balance when account status is changed
             // (inactive -> active)
@@ -45,12 +49,30 @@ function App() {
             // not fetch balance when account status is inactive
         }
     }, [isActive])
+
     const handleConnect = () => {
         metamask.activate(55556)
     }
     const handleDisconnect = () => {
         metamask.deactivate()
     }
+    const handleBuy = async () => {
+        try {
+            const signer = provider.getSigner()
+            const smartContract = new ethers.Contract(contractAddress, abi, signer)
+            const txHash = await smartContract.buy({
+                from: accounts[0],
+                value: parseUnits(reiValue, "ether")
+            })
+            smartContract.on("Buy", (from, to, tokens) => {
+                // fetch balance when buy event is emitted
+                fetchBalance()
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div>
             <AppBar position="static" color="transparent">
@@ -79,7 +101,50 @@ function App() {
                     }
                 </Toolbar>
             </AppBar>
-            { isActive && <div>{balance}</div>}
+            <Container maxWidth="sm" sx={{ mt: 2 }}>
+                { isActive &&
+                    <div>
+                        <Card>
+                            <CardContent>
+                                <Stack spacing={2}>
+                                    <Typography variant="h6" align="center">
+                                        MTK
+                                    </Typography>
+                                    <TextField
+                                        id="address"
+                                        label="Address"
+                                        value={accounts[0]}
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                    <TextField
+                                        id="balance"
+                                        label="MKT Balance"
+                                        value={balance}
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                    <Divider/>
+                                    <Typography variant="body1">
+                                        Buy MTK (1 REI = 10 MKT)
+                                    </Typography>
+                                    <TextField
+                                        required
+                                        id="outlined-required"
+                                        label="REI"
+                                        defaultValue=""
+                                        type="number"
+                                        onChange={(e) => setReiValue(e.target.value)}
+                                    />
+                                    <Button variant="contained" onClick={handleBuy}>Buy</Button>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </div>
+                }
+            </Container>
         </div>
     )
 }
